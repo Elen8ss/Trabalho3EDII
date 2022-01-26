13c076e71e6317f72e4fa7671fd55689cbd2e5b6
ElenS <elenserra080@gmail.com>
2022-01-26 12:09:38 -0300

    add dsf

--- src/structures/graph/Graph.java
+++ src/structures/graph/Graph.java
@@ -142,4 +142,52 @@ public class Graph {
 	public int numVertex ( ) {
 	    return this.numVertex;
 	}
+
+	
+    
+		static int[] visita, low, parent;
+	
+		public void DFSbridges(Graph graph){
+			Vertices vertice = new Vertices(numVertex) ;
+			visita = new int[numVertex];
+			low = new int[numVertex];
+			parent = new int[numVertex];
+			
+			for (int i=0; i<graph.numVertex(); i++) {
+				visita[i] = -1;
+				for (int j=0; j<graph.numVertex(); j++){
+					if(visita[j] == -1){//se nao foi visitado
+						parent[j] = vertice.getVertex(j); //cria uma nova raiz de aborescencia
+						
+						DFSbridgesRecursive(graph, parent[j]);
+					}
+				}
+			}
+		}
+	
+		public void DFSbridgesRecursive(Graph graph, int vertice) {
+			if(!graph.listaAdjVazia(vertice)) {
+				Edge aresta = graph.primeiroListaAdj(vertice);
+				while(aresta!=null){
+					int a = aresta.v2();
+					if (visita[a] == -1){ //se adj nao foi visitado
+						parent[a] = vertice;
+	
+						DFSbridgesRecursive(graph, a);
+						if (low[vertice] > low[a]){
+							low[vertice] = low[a];
+						}
+						if (low[a] == visita[a]){
+							System.out.println("Achou ponte!");
+						}
+					
+					}else if(a != parent[vertice] && low[vertice]>visita[a]){ //se ja foi visitado
+						low[vertice] = visita[a];
+						System.out.println("Conectado por aresta");
+					}
+	
+				}
+			}
+		}
+	
 }
